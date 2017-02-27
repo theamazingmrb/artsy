089f722675414fb0c1f4dca4e01907f30e5259dc
billie heidelberg <billie.heidelberg.jr@gmail.com>
2017-02-27 11:34:14 -0800

    adds user controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  validate :email, presence: true, uniqueness: true
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+get 'user' => 'user#index'
+get 'user/new' => 'user#new', as: :new_user
+pose 'user' => 'users#create'
 end
