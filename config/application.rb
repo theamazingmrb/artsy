eb305722cfe3686ce5f2226bead7ad14edb2a987
billie heidelberg <billie.heidelberg.jr@gmail.com>
2017-03-02 12:10:25 -0800

    fixes

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,8 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
+require 'paperclip'
+require 'aws-sdk'
 module Artsy
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
